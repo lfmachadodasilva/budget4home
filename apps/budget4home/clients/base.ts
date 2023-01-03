f9f32a48954d25a065ab48b847e86fa6c53c45f2
Luiz Felipe <lfmachadodasilva@gmail.com>
2023-01-03 21:10:05 +0000

    fetch changes #6

---
+++ apps/budget4home/clients/base.ts
@@ -0,0 +1,18 @@
+export abstract class BaseClient {
+  protected static async fetch<T>(
+    url: string,
+    token: string,
+    method: "GET" | "POST" | "PUT" | "DELETE",
+    data?: any
+  ): Promise<Response> {
+    const response = await fetch(url, {
+      method: method,
+      headers: {
+        "Content-Type": "application/json",
+        authorization: token,
+      },
+      body: JSON.stringify(data),
+    });
+    return response;
+  }
+}
