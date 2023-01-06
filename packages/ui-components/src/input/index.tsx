bdc520aaf48812903c5ca7ddd65906f5a9e99100
Luiz Felipe <lfmachadodasilva@gmail.com>
2023-01-06 15:17:29 +0000

    fix imput

--- packages/ui-components/src/input/index.tsx
+++ packages/ui-components/src/input/index.tsx
@@ -3,15 +3,17 @@ import { ComponentPropsWithRef, ForwardedRef, forwardRef } from 'react';
 import styles from './index.module.scss';
 
 interface B4hInputProp extends ComponentPropsWithRef<'input'> {
-  label: string;
+  label?: string;
 }
 
 export const B4hInput = forwardRef((props: B4hInputProp, ref: ForwardedRef<HTMLInputElement>) => {
   return (
     <div className={styles.container}>
-      <label className={styles.label} htmlFor={props.id}>
-        {props.label}
-      </label>
+      {props.label && (
+        <label className={styles.label} htmlFor={props.id}>
+          {props.label}
+        </label>
+      )}
       {props.type !== 'checkbox' && <br />}
       <input className={styles.input} ref={ref} {...props} />
     </div>
