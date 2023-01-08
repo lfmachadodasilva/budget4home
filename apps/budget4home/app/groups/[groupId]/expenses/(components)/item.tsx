8a7188e69820a40befe4529391be671ba9b61b08
Luiz Felipe <lfmachadodasilva@gmail.com>
2023-01-08 17:48:03 +0000

    remove expense type

--- apps/budget4home/app/groups/[groupId]/expenses/(components)/item.tsx
+++ apps/budget4home/app/groups/[groupId]/expenses/(components)/item.tsx
@@ -11,8 +11,6 @@ interface ExpenseItemProps {
 export const ExpenseItem = (props: ExpenseItemProps) => {
   return (
     <>
-      <label>{props.expense.type}</label>
-      {' - '}
       <label>{format(new Date(props.expense.date), 'yyyy-MM-dd')}</label>
       {' - '}
       <label>{props.expense.name}</label> - <label>{(props.expense.value / 100).toFixed(2)}</label>
