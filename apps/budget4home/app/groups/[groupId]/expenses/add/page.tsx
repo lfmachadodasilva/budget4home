3ee147dd510bf278e413b91e7fc25bdc99ceb4bd
Luiz Felipe <lfmachadodasilva@gmail.com>
2023-01-26 18:04:27 +0000

    more changes

--- apps/budget4home/app/groups/[groupId]/expenses/add/page.tsx
+++ apps/budget4home/app/groups/[groupId]/expenses/add/page.tsx
@@ -1,17 +1,12 @@
 import { ExpenseForm } from '../(components)/form';
 import { getUserId } from '../../../../../util/getUserId';
-import { groupRepository, labelRepository } from '../../../../../util/repositories';
+import { labelRepository } from '../../../../../util/repositories';
 
 export default async function ({ params }: any) {
-  let groupId = params.groupId;
-
+  const groupId = params.groupId as string;
   const userId = await getUserId();
 
-  if (!groupId) {
-    groupId = await groupRepository.getFirst(userId);
-  }
-
-  const labels = await labelRepository.getAll(userId, params.groupId);
+  const labels = await labelRepository.getAll(userId, groupId);
 
   return (
     <>
--- apps/budget4home/app/groups/[groupId]/labels/add/page.tsx
+++ apps/budget4home/app/groups/[groupId]/labels/add/page.tsx
@@ -1,18 +1,7 @@
-import { cookies } from 'next/headers';
 import { LabelForm } from '../(components)/form';
-import { firebaseAdminAuth } from '../../../../../util/firebaseAdmin';
-import { groupRepository } from '../../../../../util/repositories';
 
 export default async function ({ params }: any) {
-  let groupId = params.groupId;
-
-  if (!groupId) {
-    const nextCookies = cookies();
-    const token = nextCookies.get('token').value;
-    const { uid } = await firebaseAdminAuth.verifyIdToken(token);
-
-    groupId = await groupRepository.getFirst(uid);
-  }
+  const groupId = params.groupId as string;
 
   return (
     <>
