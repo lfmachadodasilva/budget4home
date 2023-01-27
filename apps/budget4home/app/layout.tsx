4c7ad64c0aced1b803c85d19724b121d501b1f7d
Luiz Felipe <lfmachadodasilva@gmail.com>
2023-01-27 14:18:05 +0000

    improve SEO

--- apps/budget4home/app/layout.tsx
+++ apps/budget4home/app/layout.tsx
@@ -2,12 +2,13 @@ import { Header } from './header';
 import { Providers } from './provider';
 
 import '@budget4home/ui-components/src/styles/global.scss';
+import Head from './head';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <Head />
       </head>
       <body>
         <Providers>
