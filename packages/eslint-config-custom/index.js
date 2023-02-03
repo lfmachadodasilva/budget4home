6483576a81b2517f8a606c49d364cfcffe507580
Luiz Felipe <lfmachadodasilva@gmail.com>
2023-02-03 15:34:54 +0000

    setup cra

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,5 +1,11 @@
 module.exports = {
-  extends: ['next', 'turbo', 'prettier'],
+  extends: [
+    //'next',
+    'turbo',
+    'prettier',
+    'react-app',
+    'react-app/jest'
+  ],
   rules: {
     '@next/next/no-html-link-for-pages': 'off',
     'react/jsx-key': 'off',
