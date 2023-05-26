8d01cecbfff04c0eaac3610295041f2ae59e21e9
Luiz Felipe <lfmachadodasilva@gmail.com>
2023-05-26 08:48:44 +0100

    logout if fail to auth in the backend

--- apps/budget4home/util/getUserId.ts
+++ apps/budget4home/util/getUserId.ts
@@ -1,5 +1,8 @@
+import { RedirectType } from 'next/dist/client/components/redirect';
 import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
 import { firebaseAdminAuth } from './firebaseAdmin';
+import { B4hRoutes } from './routes';
 
 export const getUserId = async () => {
   const nextCookies = cookies();
@@ -11,13 +14,13 @@ export const getUserId = async () => {
   } catch (err: any) {
     console.error(err);
 
-    if (
-      err?.errorInfo?.code === 'auth/id-token-expired' ||
-      err?.errorInfo?.code === 'auth/argument-error'
-    ) {
-      return nextCookies.get('uid')?.value;
-    }
-  }
+    // if (
+    //   err?.errorInfo?.code === 'auth/id-token-expired' ||
+    //   err?.errorInfo?.code === 'auth/argument-error'
+    // ) {
+    //   return nextCookies.get('uid')?.value;
+    // }
 
-  return null;
+    redirect(B4hRoutes.logout, RedirectType.push);
+  }
 };
