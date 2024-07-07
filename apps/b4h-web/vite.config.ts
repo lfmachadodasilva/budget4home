9e3e18304ee8d517552e313244f207dde159b7e3
Luiz Felipe <luizfelipe@Josas-MacBook-Pro.local>
2024-07-07 15:28:34 +0100

    update apps

--- apps/b4h-web/vite.config.ts
+++ apps/b4h-web/vite.config.ts
@@ -1,19 +1,19 @@
 /// <reference types='vitest' />
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   root: __dirname,
   cacheDir: '../../node_modules/.vite/apps/b4h-web',
 
   server: {
-    port: 4200,
+    port: 3000,
     host: 'localhost',
   },
 
   preview: {
-    port: 4300,
+    port: 3100,
     host: 'localhost',
   },
 
--- packages/models/jest.config.ts
+++ packages/models/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
-  displayName: 'b4h-api',
+  displayName: 'models',
   preset: '../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/apps/b4h-api',
+  coverageDirectory: '../../coverage/packages/models',
 };
