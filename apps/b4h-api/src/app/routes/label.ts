110305466d2f898dff54e5eea623aaac47dd5c10
Luiz Felipe Machado da Silva <lfmachadodasilva@gmail.com>
2024-07-27 07:05:08 +0100

    upgrade yarn

--- apps/b4h-api/src/app/routes/label.ts
+++ apps/b4h-api/src/app/routes/label.ts
@@ -2,7 +2,7 @@ import { FastifyInstance } from 'fastify';
 import { getLabels } from '../repositories/label/labelRepository';
 
 export default async function (fastify: FastifyInstance) {
-  fastify.get('/labels', async function (request, reply) {
+  fastify.get('/api/labels', async function (request, reply) {
     return await getLabels('1', '1');
   });
 }
