c4f9cf0a8a8cadcb6e61071b4987a9aba9a84701
Luiz Felipe <lfmachadodasilva@gmail.com>
2024-08-02 05:42:10 +0100

    test vercel

--- apps/b4h-api/src/main.ts
+++ apps/b4h-api/src/main.ts
@@ -12,12 +12,15 @@ const server = Fastify({
 // Register your application as a normal plugin.
 server.register(app);
 
-// Start listening.
-server.listen({ port, host }, err => {
-  if (err) {
-    server.log.error(err);
-    process.exit(1);
-  } else {
-    console.log(`[ ready ] http://${host}:${port}`);
-  }
-});
+const start = () => {
+  // Start listening.
+  server.listen({ port, host }, err => {
+    if (err) {
+      server.log.error(err);
+      process.exit(1);
+    } else {
+      console.log(`[ ready ] http://${host}:${port}`);
+    }
+  });
+};
+start();
