43591fc6fb25996a9b10af948e760f769b6d30ff
Luiz Felipe <lfmachadodasilva@gmail.com>
2024-08-02 05:45:14 +0100

    Revert "test vercel"
    This reverts commit c4f9cf0a8a8cadcb6e61071b4987a9aba9a84701.

--- apps/b4h-api/src/main.ts
+++ apps/b4h-api/src/main.ts
@@ -12,15 +12,12 @@ const server = Fastify({
 // Register your application as a normal plugin.
 server.register(app);
 
-const start = () => {
-  // Start listening.
-  server.listen({ port, host }, err => {
-    if (err) {
-      server.log.error(err);
-      process.exit(1);
-    } else {
-      console.log(`[ ready ] http://${host}:${port}`);
-    }
-  });
-};
-start();
+// Start listening.
+server.listen({ port, host }, err => {
+  if (err) {
+    server.log.error(err);
+    process.exit(1);
+  } else {
+    console.log(`[ ready ] http://${host}:${port}`);
+  }
+});
