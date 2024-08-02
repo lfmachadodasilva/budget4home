8fd292d894d4945e2fc77bb02d69f73d50136acb
Luiz Felipe <lfmachadodasilva@gmail.com>
2024-08-02 05:25:23 +0100

    test vercel

--- apps/b4h-api/src/main.ts
+++ apps/b4h-api/src/main.ts
@@ -21,3 +21,8 @@ server.listen({ port, host }, err => {
     console.log(`[ ready ] http://${host}:${port}`);
   }
 });
+
+export default async (req, res) => {
+  await server.ready();
+  server.server.emit('request', req, res);
+};
