f4d045c1eae88447a6279734c0af6940d0fb874b
Luiz Felipe <lfmachadodasilva@gmail.com>
2024-08-05 21:49:51 +0100

    update variable

--- apps/b4h-next/src/clients/auth.ts
+++ apps/b4h-next/src/clients/auth.ts
@@ -3,15 +3,10 @@ import { B4hBaseHeaders } from '@/shared/headers';
 import { B4hMethod } from '@/shared/method';
 import { RequestCookie } from 'next/dist/compiled/@edge-runtime/cookies';
 
-const baseUrl = (process.env['API_URL'] as string) ?? 'http://localhost:3000';
+const baseUrl = (process.env['NEXT_PUBLIC_API_URL'] as string) ?? 'http://localhost:3000';
 
-export const loginFetch = async (token: string) => {
-  console.log('clients>auth', {
-    baseUrl,
-    processEnv: process.env['API_URL'],
-    url: new URL(B4hApiRoutes.login, baseUrl)
-  });
-  return fetch(new URL(B4hApiRoutes.login, baseUrl), {
+export const loginFetch = async (token: string) =>
+  fetch(new URL(B4hApiRoutes.login, baseUrl), {
     method: B4hMethod.post,
     headers: {
       ...B4hBaseHeaders,
@@ -19,7 +14,6 @@ export const loginFetch = async (token: string) => {
     },
     cache: 'no-cache'
   });
-};
 
 export const refreshSessionFetch = async (requestUrl: string, session: RequestCookie | undefined) =>
   await fetch(new URL(B4hApiRoutes.login, requestUrl), {
