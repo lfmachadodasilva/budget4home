d773e03c41bfb4650f6b4a40fb58e7ced10f0c73
Luiz Felipe <lfmachadodasilva@gmail.com>
2024-08-25 13:21:41 +0100

    standalone

--- apps/b4h-next/next.config.js
+++ apps/b4h-next/next.config.js
@@ -8,6 +8,7 @@ const { composePlugins, withNx } = require('@nx/next');
  **/
 const nextConfig = {
   distDir: '../../dist/apps/b4h-next/.next',
+  output: 'standalone',
   nx: {
     // Set this to true if you would like to use SVGR
     // See: https://github.com/gregberge/svgr
