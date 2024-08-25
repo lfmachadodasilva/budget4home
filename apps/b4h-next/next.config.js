87e3774d8cd8bff5fc9eff31d871fbdc492ae70d
Luiz Felipe <lfmachadodasilva@gmail.com>
2024-08-25 13:26:59 +0100

    add stuff

--- apps/b4h-next/next.config.js
+++ apps/b4h-next/next.config.js
@@ -3,6 +3,16 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { composePlugins, withNx } = require('@nx/next');
 
+const imagesDomains = [
+  'firebasestorage.googleapis.com',
+  'github.com',
+  'budget4home.vercel.app',
+  'b4h.vercel.app'
+];
+if (process.env.NODE_ENV === 'development') {
+  imagesDomains.push('localhost');
+}
+
 /**
  * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
  **/
@@ -13,6 +23,9 @@ const nextConfig = {
     // Set this to true if you would like to use SVGR
     // See: https://github.com/gregberge/svgr
     svgr: false
+  },
+  images: {
+    domains: imagesDomains
   }
 };
 
