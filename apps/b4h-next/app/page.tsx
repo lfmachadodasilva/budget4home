279bdafe6789f6c45f2300b8c0c2808feb3f1a2f
Luiz Felipe Machado da Silva <lfmachadodasilva@gmail.com>
2024-08-26 12:39:09 +0100

    update metadata

--- apps/b4h-next/app/page.tsx
+++ apps/b4h-next/app/page.tsx
@@ -3,6 +3,10 @@ import styles from './page.module.scss';
 import { models } from '@b4h/models';
 import { WebComponents } from '@b4h/web-components';
 
+export const metadata = {
+  title: 'home | budget4home'
+};
+
 export default function Index() {
   return (
     <div className={styles.page}>
