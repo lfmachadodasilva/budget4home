92455b783f50c7b8c4fcc6af749beb6f2a978bd9
Luiz Felipe Machado da Silva <lfmachadodasilva@gmail.com>
2024-08-26 12:31:42 +0100

    fix

--- apps/b4h-next/app/page.tsx
+++ apps/b4h-next/app/page.tsx
@@ -1,15 +1,11 @@
 import styles from './page.module.scss';
 
-import { firebase } from '@b4h/firebase';
-import { firebaseAdmin } from '@b4h/firebase-admin';
 import { models } from '@b4h/models';
 import { WebComponents } from '@b4h/web-components';
 
 export default function Index() {
   return (
     <div className={styles.page}>
-      {firebase()}
-      {firebaseAdmin()}
       {models()}
       <WebComponents />
     </div>
