01365da3187310af09e1e5b142491503db1c8c68
Luiz Felipe Machado da Silva <lfmachadodasilva@gmail.com>
2024-08-26 12:29:33 +0100

    firestore

--- libs/firebase-admin/src/lib/firebase-admin.spec.ts
+++
@@ -1,7 +0,0 @@
-import { firebaseAdmin } from './firebase-admin';
-
-describe('firebaseAdmin', () => {
-  it('should work', () => {
-    expect(firebaseAdmin()).toEqual('firebase-admin');
-  });
-});
--- libs/firebase/src/lib/firebase.spec.ts
+++
@@ -1,7 +0,0 @@
-import { firebase } from './firebase';
-
-describe('firebase', () => {
-  it('should work', () => {
-    expect(firebase()).toEqual('firebase');
-  });
-});
