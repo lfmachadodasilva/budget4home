77f7ec29b27e1cf91265029419348fb845c455a5
Luiz Felipe Machado da Silva <lfmachadodasilva@gmail.com>
2024-10-12 11:01:56 +0100

    change expense loading

---
+++ apps/b4h-next/src/app/expenses/items.tsx
@@ -0,0 +1,58 @@
+import { getExpensesFirebase, getGroupsFirestore, getLabelsFirestore } from '@b4h/firestore';
+import Link from 'next/link';
+import { B4hItem } from '../../components/ui/item/item';
+import {
+  B4hExpenseHeaderType,
+  expensesByDate,
+  expensesByLabel,
+  formatValue,
+  getDateFromQuery
+} from '../../utils/expenses';
+import { useB4hSession } from '../../utils/hooks/useB4hSession';
+import { labelsById } from '../../utils/label';
+import { B4hRoutes } from '../../utils/routes';
+
+export interface B4hExpensesItemsProps {
+  searchParams: B4hExpenseHeaderType;
+}
+
+export const B4hExpensesItems = async ({ searchParams }: B4hExpensesItemsProps) => {
+  const { userId } = useB4hSession();
+  const date = getDateFromQuery(searchParams);
+
+  // fetch data
+  const groups = await getGroupsFirestore(userId);
+  const [labels, expenses] = await Promise.all([
+    getLabelsFirestore(userId, groups[0].id),
+    getExpensesFirebase(userId, groups[0].id, date)
+  ]);
+
+  // format data
+  const labelById = labelsById(labels);
+  const expenseBy =
+    searchParams.viewBy === 'byLabel'
+      ? expensesByLabel(expenses, labelById)
+      : expensesByDate(expenses);
+
+  return Object.entries(expenseBy).map(([key, expenses]) => (
+    <B4hItem.Group key={key}>
+      <B4hItem.GroupTitle>
+        <p>{key}</p>
+        <p>{formatValue(expenses.reduce((acc, expense) => acc + expense.value, 0))}</p>
+      </B4hItem.GroupTitle>
+
+      <B4hItem.Items>
+        {expenses.map(expense => (
+          <Link href={`${B4hRoutes.expenses}/${expense.id}`} key={expense.id}>
+            <B4hItem.Item>
+              <p>
+                {labelById[expense.label]?.icon} {expense.name}
+              </p>
+              <p>{formatValue(expense.value)}</p>
+            </B4hItem.Item>
+          </Link>
+        ))}
+      </B4hItem.Items>
+    </B4hItem.Group>
+  ));
+};
--- apps/b4h-next/src/app/expenses/page.tsx
+++ apps/b4h-next/src/app/expenses/page.tsx
@@ -1,41 +1,20 @@
-import { getExpensesFirebase, getGroupsFirestore, getLabelsFirestore } from '@b4h/firestore';
 import Link from 'next/link';
+import { Suspense } from 'react';
+import { B4hLoading } from '../../components/loading';
 import { B4hButton } from '../../components/ui/button/button';
 import { B4hItem } from '../../components/ui/item/item';
 import { B4hPageLayout } from '../../components/ui/layout/layout';
-import {
-  B4hExpenseHeaderType,
-  expensesByDate,
-  expensesByLabel,
-  formatValue,
-  getDateFromQuery
-} from '../../utils/expenses';
-import { useB4hSession } from '../../utils/hooks/useB4hSession';
-import { labelsById } from '../../utils/label';
+import { B4hExpenseHeaderType } from '../../utils/expenses';
 import { B4hRoutes } from '../../utils/routes';
 import { B4hExpensesHeader } from './header';
+import { B4hExpensesItems } from './items';
 
 export const metadata = {
   title: 'expenses | budget4home'
 };
 
 export default async function Expeses({ searchParams }: { searchParams: B4hExpenseHeaderType }) {
-  const date = getDateFromQuery(searchParams);
-  const { userId } = useB4hSession();
-
-  // fetch data
-  const groups = await getGroupsFirestore(userId);
-  const [labels, expenses] = await Promise.all([
-    getLabelsFirestore(userId, groups[0].id),
-    getExpensesFirebase(userId, groups[0].id, date)
-  ]);
-
-  // format data
-  const labelById = labelsById(labels);
-  const expenseBy =
-    searchParams.viewBy === 'byLabel'
-      ? expensesByLabel(expenses, labelById)
-      : expensesByDate(expenses);
+  const key = JSON.stringify(searchParams);
 
   return (
     <B4hPageLayout.Root>
@@ -47,29 +26,10 @@ export default async function Expeses({ searchParams }: { searchParams: B4hExpen
       </B4hPageLayout.Header>
       <B4hPageLayout.Content>
         <B4hExpensesHeader />
-
         <B4hItem.Root>
-          {Object.entries(expenseBy).map(([key, expenses]) => (
-            <B4hItem.Group key={key}>
-              <B4hItem.GroupTitle>
-                <p>{key}</p>
-                <p>{formatValue(expenses.reduce((acc, expense) => acc + expense.value, 0))}</p>
-              </B4hItem.GroupTitle>
-
-              <B4hItem.Items>
-                {expenses.map(expense => (
-                  <Link href={`${B4hRoutes.expenses}/${expense.id}`} key={expense.id}>
-                    <B4hItem.Item>
-                      <p>
-                        {labelById[expense.label]?.icon} {expense.name}
-                      </p>
-                      <p>{formatValue(expense.value)}</p>
-                    </B4hItem.Item>
-                  </Link>
-                ))}
-              </B4hItem.Items>
-            </B4hItem.Group>
-          ))}
+          <Suspense key={key} fallback={<B4hLoading />}>
+            <B4hExpensesItems searchParams={searchParams} />
+          </Suspense>
         </B4hItem.Root>
       </B4hPageLayout.Content>
     </B4hPageLayout.Root>
