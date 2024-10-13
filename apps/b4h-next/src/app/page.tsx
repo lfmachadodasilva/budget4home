afe7f68b54bff5026583cd320b61570fcbdbddff
Luiz Felipe <lfmachadodasilva@gmail.com>
2024-10-13 11:37:57 +0100

    add try catch summry at home

--- apps/b4h-next/src/app/page.tsx
+++ apps/b4h-next/src/app/page.tsx
@@ -1,3 +1,7 @@
+import { b4hSession } from '@/utils/session';
+import { getExpensesFirebase } from '@b4h/firestore';
+import { ExpenseModel } from '@b4h/models';
+import { B4hExpenseSummary } from './expenses/(components)/summary';
 import styles from './page.module.scss';
 
 export const metadata = {
@@ -5,21 +9,29 @@ export const metadata = {
 };
 
 export default async function Home() {
-  // const { userId, getFavoriteGroupId } = b4hSession();
-  // const groupId = await getFavoriteGroupId();
-  // const expenses = groupId && (await getExpensesFirebase(userId, groupId));
+  const { userId, getFavoriteGroupId } = b4hSession();
+
+  let expenses: ExpenseModel[] | undefined | null;
+  try {
+    const groupId = await getFavoriteGroupId();
+    if (groupId) {
+      expenses = await getExpensesFirebase(userId, groupId);
+    }
+  } catch (error) {
+    console.error(error);
+  }
+
   return (
     <div className={styles.page}>
       <h1>home</h1>
       <p>welcome to budget4home!</p>
-      {/* <br />
+      <br />
       {expenses && (
         <>
           <p>summary of your ⭐️ group</p>
           <B4hExpenseSummary expenses={expenses} />
         </>
       )}
-      */}
     </div>
   );
 }
