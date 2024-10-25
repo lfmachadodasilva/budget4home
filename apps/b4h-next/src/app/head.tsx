b74f000c4f17dd72e36e36772a97b1d4dd9a6f74
Luiz Felipe <lfmachadodasilva@gmail.com>
2024-10-25 08:13:32 +0100

    update manifest

--- apps/b4h-next/src/app/head.tsx
+++ apps/b4h-next/src/app/head.tsx
@@ -9,8 +9,6 @@ export default function B4hHead() {
       <link rel="shortcut icon" href="/favicon.ico" />
       <link rel="icon" type="image/x-icon" href="/favicon.ico" />
       <link rel="apple-touch-icon" href="/logo213.png"></link>
-      <link rel="manifest" href="/manifest.json" crossOrigin="use-credentials" />
-
       <meta name="theme-color" content="#612F74" />
 
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
