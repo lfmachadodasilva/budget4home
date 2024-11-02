77dc4bab6165a03221a61869c015efd5a26d40e4
Luiz Felipe <lfmachadodasilva@gmail.com>
2024-11-02 21:05:17 +0000

    preview

--- apps/b4h-next/src/utils/config.ts
+++ apps/b4h-next/src/utils/config.ts
@@ -2,8 +2,8 @@ export const BASE_URL = (process.env['NEXT_PUBLIC_API_URL'] as string) ?? 'http:
 
 console.log('BASE_URL', {
   baseURL: BASE_URL,
-  vercelEnv: process.env['VERCEL_ENV'],
-  prodUrl: process.env['VERCEL_PROJECT_PRODUCTION_URL'],
-  vercelUrl: process.env['VERCEL_URL'],
-  branchUrl: process.env['VERCEL_BRANCH_URL']
+  vercelEnv: process.env['VERCEL_ENV'] as string,
+  prodUrl: process.env['VERCEL_PROJECT_PRODUCTION_URL'] as string,
+  vercelUrl: process.env['VERCEL_URL'] as string,
+  branchUrl: process.env['VERCEL_BRANCH_URL'] as string
 });
