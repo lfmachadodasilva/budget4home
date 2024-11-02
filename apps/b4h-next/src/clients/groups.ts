ddc1436be9d2783d0b4b5926a8c51606655bf141
Luiz Felipe Machado da Silva <lfmachadodasilva@gmail.com>
2024-11-02 21:24:42 +0000

    dynamic url (#123)

--- apps/b4h-next/src/clients/groups.ts
+++ apps/b4h-next/src/clients/groups.ts
@@ -1,14 +1,14 @@
+import { BASE_URL } from '@/utils/config';
 import { FETCH_GROUPS, FETCH_REVALIDATE_TIME, SESSION } from '@/utils/constants';
 import { B4hApiRoutes } from '@/utils/routes';
 import { GroupModel } from '@b4h/models';
 import { cookies } from 'next/headers';
 
-const baseUrl = (process.env['NEXT_PUBLIC_API_URL'] as string) ?? 'http://localhost:3000';
 export const fetchGroups = async (): Promise<GroupModel[] | null> => {
   const cookie = await cookies();
   const session = cookie.get(SESSION)?.value || '';
 
-  const res = await fetch(new URL(B4hApiRoutes.groups, baseUrl), {
+  const res = await fetch(new URL(B4hApiRoutes.groups, BASE_URL), {
     method: 'GET',
     headers: {
       session: session
