79ae930529ddfcddd5e769b91dbffd951d69f8a8
Luiz Felipe <lfmachadodasilva@gmail.com>
2024-11-03 09:48:12 +0000

    extend vercel analytics

--- apps/b4h-next/src/app/expenses/analytics.tsx
+++ apps/b4h-next/src/app/expenses/analytics.tsx
@@ -1,27 +1,37 @@
 'use client';
 
 import { getFirebaseAnalytics, logEvent } from '@b4h/firebase';
+import { track } from '@vercel/analytics/react';
+
+const ExpensesAnalyticsKeys = {
+  expenses: 'expenses',
+  addExpense: 'add_expense',
+  updateExpense: 'update_expense'
+};
 
 export const ExpensesAnalytics = () => {
   getFirebaseAnalytics().then(analytics => {
-    analytics && logEvent(analytics, 'expenses');
+    analytics && logEvent(analytics, ExpensesAnalyticsKeys.expenses);
   });
+  track(ExpensesAnalyticsKeys.expenses);
 
   return null;
 };
 
 export const AddExpenseAnalytics = () => {
   getFirebaseAnalytics().then(analytics => {
-    analytics && logEvent(analytics, 'add_expense');
+    analytics && logEvent(analytics, ExpensesAnalyticsKeys.addExpense);
   });
+  track(ExpensesAnalyticsKeys.addExpense);
 
   return null;
 };
 
 export const UpdateExpenseAnalytics = () => {
   getFirebaseAnalytics().then(analytics => {
-    analytics && logEvent(analytics, 'update_expense');
+    analytics && logEvent(analytics, ExpensesAnalyticsKeys.updateExpense);
   });
+  track(ExpensesAnalyticsKeys.updateExpense);
 
   return null;
 };
