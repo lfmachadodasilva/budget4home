95caa8d9b308ad70f600db91dd468a4011f17a0b
Luiz Felipe <lfmachadodasilva@gmail.com>
2025-02-07 18:18:23 +0000

    debug in prod

--- apps/b4h-next/src/utils/config.ts
+++ apps/b4h-next/src/utils/config.ts
@@ -1,12 +1,16 @@
-const HTTPS = 'https://';
-export const BASE_URL =
+function ensureHttps(url: string): string {
+  const https = 'https://';
+  return url.startsWith(https) ? url : https + url;
+}
+export const BASE_URL = ensureHttps(
   process.env['VERCEL_ENV'] === 'production'
     ? (process.env['VERCEL_PROJECT_PRODUCTION_URL'] as string) // use production url
     : process.env['VERCEL_ENV'] === 'development'
-      ? HTTPS + (process.env['NEXT_PUBLIC_API_URL'] as string) // use developer url
+      ? (process.env['NEXT_PUBLIC_API_URL'] as string) // use developer url
       : process.env['VERCEL_ENV'] === 'preview'
-        ? HTTPS + (process.env['NEXT_PUBLIC_API_URL'] as string) // use preview url
-        : 'http://localhost:3000'; // use local url
+        ? (process.env['NEXT_PUBLIC_API_URL'] as string) // use preview url
+        : 'http://localhost:3000' // use local url
+);
 
 console.log('BASE_URL', {
   BASE_URL,
