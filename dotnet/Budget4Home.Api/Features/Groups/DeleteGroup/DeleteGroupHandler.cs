3d96a070f29f2c5659e7b7faeb538adc9f997f65
Luiz Silva <luiz.silva@paymentsense.com>
2025-05-18 09:04:41 +0100

    delete cascade

--- dotnet/Budget4Home.Api/Features/Groups/DeleteGroup/DeleteGroupHandler.cs
+++ dotnet/Budget4Home.Api/Features/Groups/DeleteGroup/DeleteGroupHandler.cs
@@ -1,6 +1,8 @@
 using Budget4Home.Api.Attributes;
 using Budget4Home.Api.Configuration.Auth;
 using Budget4Home.Api.Configuration.Exceptions;
+using Budget4Home.Api.Features.Expenses.DeleteExpense;
+using Budget4Home.Api.Features.Labels.DeleteLabel;
 using Budget4Home.Mongo.Models;
 using MongoDB.Bson;
 using MongoDB.Driver;
@@ -8,10 +10,19 @@ using MongoDB.Driver;
 namespace Budget4Home.Api.Features.Groups.DeleteGroup;
 
 [AutoRegister(typeof(DeleteGroupHandler), Scope = ServiceScope.Scoped)]
-public class DeleteGroupHandler(AuthContext authContext, IMongoCollection<GroupDocument> collection)
+public class DeleteGroupHandler(
+    AuthContext authContext, 
+    DeleteLabelHandler deleteLabelHandler,
+    DeleteExpenseHandler deleteExpenseHandler,
+    IMongoCollection<GroupDocument> collection)
 {
     public async Task RunAsync(string groupId, CancellationToken cancellationToken)
     {
+        await Task.WhenAll(
+            deleteLabelHandler.RunAsync(groupId, cancellationToken),
+            deleteExpenseHandler.RunAsync(groupId, cancellationToken)
+        );
+        
         var result = await collection.DeleteOneAsync(
             Builders<GroupDocument>.Filter.And(
                 Builders<GroupDocument>.Filter.Eq(x => x.Id, ObjectId.Parse(groupId)), 
